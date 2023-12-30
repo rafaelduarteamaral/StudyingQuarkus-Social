dad4922c92970116f6e0216a7f161e5d0ba849dd
Rafael Duarte Amaral <rafaelduarteamaral@gmail.com>
2023-12-30 12:02:56 -0300

    feat: Studying java quarkus

---
+++ src/main/java/io/github/rafaelamaral/quarkussocial/rest/dto/FollowersPerUserResponse.java
@@ -0,0 +1,11 @@
+package io.github.rafaelamaral.quarkussocial.rest.dto;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class FollowersPerUserResponse {
+    private Integer followersCount;
+    private List<FollowerResponse> content;
+}
